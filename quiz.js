443dadce0b9144e703307d7d33a16a380ceddd51
vishvapatel2529 <email_2529vishva@gmail.com>
2021-03-27 14:31:59 -0500

    added event handler

--- quiz.js
+++ quiz.js
@@ -1,31 +1,36 @@
-const myQuestions = [
-  {
-    prompt: "2+2?,     (a). 5    (b). 7     (c). 4",
-    correctAnswer: "c",
-  },
-  {
-    prompt: "5*5?,    a: 22,    b: 35,   c: 25",
+document.querySelector(".s2").addEventListener("click", function () {
+  // console.log(myQuestions.length);
+  //console.log(document.querySelector(".s1").value);
 
-    correctAnswer: "c",
-  },
-  {
-    prompt:
-      "what color are apples?,    a: blue,    b: black,    c: orange,    d: red",
+  const myQuestions = [
+    {
+      prompt: "2+2?,     (a). 5    (b). 7     (c). 4",
+      correctAnswer: "c",
+    },
+    {
+      prompt: "5*5?,    a: 22,    b: 35,   c: 25",
 
-    correctAnswer: "d",
-  },
-];
+      correctAnswer: "c",
+    },
+    {
+      prompt:
+        "what color are apples?,    a: blue,    b: black,    c: orange,    d: red",
 
-var score = 0;
+      correctAnswer: "d",
+    },
+  ];
 
-for (var i = 0; i < myQuestions.length; i++) {
-  var response = window.prompt(myQuestions[i].prompt);
-  if (response == myQuestions[i].correctAnswer) {
-    score++;
-    alert("correct answer");
-  } else {
-    alert("wrong answer");
+  var score = 0;
+
+  for (var i = 0; i < myQuestions.length; i++) {
+    var response = window.prompt(myQuestions[i].prompt);
+    if (response == myQuestions[i].correctAnswer) {
+      score++;
+      alert("correct answer");
+    } else {
+      alert("wrong answer");
+    }
   }
-}
 
-alert("you got " + score + " / " + myQuestions.length);
+  alert("you got " + score + " / " + myQuestions.length);
+});
